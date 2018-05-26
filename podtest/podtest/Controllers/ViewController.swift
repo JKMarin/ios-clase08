1154e9e553ba1c9023db77877673ded4c74cf9de
JKMarin <marin.stcruz@gmail.com>
2018-05-26 10:32:06 -0700

    Trabajo en clase 5

---
+++ podtest/podtest/Controllers/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  podtest
+//
+//  Created by user139236 on 5/26/18.
+//  Copyright © 2018 JCMA. All rights reserved.
+//
+
+import UIKit
+import SVProgressHUD
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dataView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //SVProgressHUD.show()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func getDataPressed(_ sender: UIButton) {
+        APIManager.sharedInstance.getPostWithId(postId: 1, onSuccess: { json in
+            DispatchQueue.main.async {
+                self.dataView?.text = String(describing: json)
+            }
+        }, onFailure: { error in
+            let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+            self.show(alert, sender: nil)
+        })
+        
+    }
+   
+    @IBAction func getUsersPressed(_ sender: Any) {
+        APIManager.sharedInstance.getUsers(onSuccess: { json in
+            DispatchQueue.main.async {
+                self.dataView?.text = String(describing: json)
+            }
+        }, onFailure: { error in
+            let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+            self.show(alert, sender: nil)
+        })    }
+    
+    @IBAction func getCommentsPressed(_ sender: Any) {
+        APIManager.sharedInstance.getComments(onSuccess: { json in
+            DispatchQueue.main.async {
+                self.dataView?.text = String(describing: json)
+            }
+        }, onFailure: { error in
+            let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+            self.show(alert, sender: nil)
+        })     }
+}
+
